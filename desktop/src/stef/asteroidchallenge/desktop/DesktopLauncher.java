ad4a9b418978aceba1460fd575bcb8f725c2b0c5
Stefan Stefanovic <stefan.stefanovic.office@gmail.com>
2021-04-15 17:20:27 +0200

    Make app full screen

--- desktop/src/stef/asteroidchallenge/desktop/DesktopLauncher.java
+++ desktop/src/stef/asteroidchallenge/desktop/DesktopLauncher.java
@@ -9,6 +9,8 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+		//set this to false for windowed mode
+		config.fullscreen = true;
 		new LwjglApplication(new AsteroidGame(config.width, config.height), config);
 	}
 }
